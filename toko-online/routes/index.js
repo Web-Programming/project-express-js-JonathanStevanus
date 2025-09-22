07717673494021405d853357ef3386553c041280
JonathanStevanus <jonathanstv1343@gmail.com>
2025-09-22 17:02:58 +0700

    pertemuan 4 bootstrap
    .

---
+++ toko-online/routes/index.js
@@ -0,0 +1,14 @@
+// var express = require('express');
+// var router = express.Router();
+// var products = require('../data/products');
+
+
+/* GET home page. */
+<% layout('layout') %>
+// router.get('/', function(req, res, next) {
+//   res.render('index', { 
+//     title: 'Toko Online Sederhana',
+//     products:products });
+// });
+
+// module.exports = router;
