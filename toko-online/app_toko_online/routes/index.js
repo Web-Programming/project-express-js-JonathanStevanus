95bd11791f999dba45e4cd01ab6084347f0fa2a9
JonathanStevanus <jonathanstv1343@gmail.com>
2025-09-29 16:55:28 +0700

    pertemuan 6 29-September

--- toko-online/app_toko_online/routes/index.js
+++ toko-online/app_toko_online/routes/index.js
@@ -1,14 +1,16 @@
  var express = require('express');
  var router = express.Router();
- var products = require('../data/products');
+var controllerMain = require('../controllers/main');
 
 
 /* GET home page. */
- router.get('/', function(req, res, next) {
-   res.render('index', { 
-     title: 'Toko Online Sederhana',
-     products:products });
- });
+//  router.get('/', function(req, res, next) {
+//    res.render('index', { 
+//      title: 'Toko Online Sederhana',
+//      products:products });
+//  });
+
+router.get('/', controllerMain.index);
 
  router.get('/search', function(req, res, next) {
     const query = req.query.q;
